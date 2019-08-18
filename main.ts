642d99e71b08cfd1820d640e3241981ebb36ae74
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-08-17 21:16:25 -0400

    Add the comment block

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Custom blocks from the series MakeCode Beyond the LED
+ * by David Sparks
+ */
+//% weight=60 color=#ff8c00 icon="\uf085"
+namespace gearbox {
+    /**
+     * Write a comment
+     * @param theComment eg:comment
+     */
+    //% block=//|%theComment
+    export function comment(theComment: string): void {
+        // do nothing
+    }
+}
