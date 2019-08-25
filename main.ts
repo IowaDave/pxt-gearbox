f750b6d986616453734fae3afdf5713d4cd272da
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2019-08-24 22:51:10 -0500

    Add groups

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@
  * by David Sparks
  */
 //% weight=60 color=#ff8c00 icon="\uf085"
+//% groups=['others', 'Constants', 'Booleans', 'Functions']
 namespace gearbox {
     /**
      * Write a comment
@@ -23,6 +24,7 @@ namespace gearbox {
      * return number of radians per one degree
      */
     //% block="radiansPerOneDegree"
+    //% group="Constants"
     export function radiansPerDegree(): number {
         return 2 * Math.PI /360;
     }
@@ -30,6 +32,7 @@ namespace gearbox {
      * return number of degrees per one radian
      */
     //% block="degreesPerOneRadian"
+    //% group="Constants"
     export function degreesPerRadian(): number {
         return 360/(2 * Math.PI);
     }
@@ -38,6 +41,7 @@ namespace gearbox {
      * @param radians eg:1
      */
     //% block=convert|%radians|radians|to|heading
+    //% group="Functions"
     export function rad2head(radians: number): number {
         let degrees = radians * degreesPerRadian();
         if (degrees > 90) {
@@ -52,6 +56,7 @@ namespace gearbox {
      * @param x eg:1
      */
     //% block="convert coordinates x=|%x|and y=|%y| to heading"
+    //% group="Functions"
     export function coords2head(x: number, y:number): number {
         if (x == 0) {
             if (y == 0) {
@@ -66,10 +71,12 @@ namespace gearbox {
         return rad2head(Math.atan2(y, x));
     }
     /**
-     * return a number rounded to precision
+     * return a number rounded to user-specified
+     * precision of 0 to 15 decimal places
      * @param precision eg:2
      */
     //% block=round|%value|to|%precision|places
+    //% group="Functions"
     export function round(value: number, precision:number): number {
         return Math.roundWithPrecision(value, precision);
     }
